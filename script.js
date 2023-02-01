ab87dda433f4e32138174e8e7feb244654c19e14
DustUnderBag <61408175+DustUnderBag@users.noreply.github.com>
2023-02-02 02:10:16 +0800

    Create function return user's option from prompt

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function getComputerSelection() {
   else if(choiceId > 0.33 && choiceId < 0.66) return paper;
   else return scissors;
 }
+
+function getPlayerSelection(playerInput) {
+  playerInput = prompt("Enter rock/paper/scissors to play against me!");
+  playerInput = playerInput.toLowerCase();
+  
+  if(playerInput === rock ||
+   playerInput === paper ||
+   playerInput === scissors) {
+   return playerInput; 
+  }else {
+    console.log(`Invalid input`);
+    getPlayerSelection();
+  }
+}
